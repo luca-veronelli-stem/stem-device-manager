17aeab5e7f1a1d89a281f4e0e30f66181c3f19c9
Paperbat <pignedoli-m@libero.it>
2024-06-27 09:38:43 +0200

    Aggiornato gitignore

--- obj/Debug/net8.0-windows/STEMPC.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/STEMPC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STEMPC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f3caec0912f922dabd49684e63ca8686d0411d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee73eddfb3449960d5e0696bd437c020484e3db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("STEMPC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STEMPC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0-windows/STEMPC.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/STEMPC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STEMPC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6b69db8f0cdd8dc406c9049f0f4a293ad6a236")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee73eddfb3449960d5e0696bd437c020484e3db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("STEMPC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STEMPC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
