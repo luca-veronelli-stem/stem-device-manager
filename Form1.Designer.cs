48f3caec0912f922dabd49684e63ca8686d0411d
Paperbat <pignedoli-m@libero.it>
2024-06-27 08:31:54 +0200

    Prima versione compilante con classe terminale generata da chatgpt

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace StemPC
+{ 
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tabControl = new TabControl();
+            tabPage1 = new TabPage();
+            Terminal = new RichTextBox();
+            tabPage2 = new TabPage();
+            tabControl.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl
+            // 
+            tabControl.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tabControl.Controls.Add(tabPage1);
+            tabControl.Controls.Add(tabPage2);
+            tabControl.Location = new Point(12, 12);
+            tabControl.Name = "tabControl";
+            tabControl.SelectedIndex = 0;
+            tabControl.Size = new Size(776, 426);
+            tabControl.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(Terminal);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(768, 398);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Debug";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Terminal
+            // 
+            Terminal.BackColor = SystemColors.WindowFrame;
+            Terminal.Dock = DockStyle.Fill;
+            Terminal.ForeColor = SystemColors.Info;
+            Terminal.Location = new Point(3, 3);
+            Terminal.Name = "Terminal";
+            Terminal.Size = new Size(762, 392);
+            Terminal.TabIndex = 0;
+            Terminal.Text = "";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(768, 398);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Init";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "STEM Protocol Companion";
+            tabControl.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private RichTextBox Terminal;
+    }
+}
---
+++ Terminal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace StemPC
+{
+    public class Terminal
+    {
+        private StringBuilder _log;
+
+        public Terminal()
+        {
+            _log = new StringBuilder();
+        }
+
+        public void WriteLine(string message)
+        {
+            _log.AppendLine(message);
+            Console.WriteLine(message);
+        }
+
+        public string GetLog()
+        {
+            return _log.ToString();
+        }
+    }
+}
