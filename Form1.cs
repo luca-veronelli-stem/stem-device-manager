ea568d93174807db0f5d6c3efb7c3f1b874e0fe9
Paperbat <pignedoli-m@libero.it>
2025-05-21 10:19:56 +0200

    Cambio di nomi dei file di basso livello

--- Form1.cs
+++ Form1.cs
@@ -223,7 +223,7 @@ namespace StemPC
                 //TOPLIFT devices
                    new DeviceInfo(0x000803C1, "Keyboard 1", true),
                    new DeviceInfo(0x000803C2, "Keyboard 2", true),
-                   //new DeviceInfo(0x00030103, "Keyboard 3"),
+         //          new DeviceInfo(0x000803C3, "Keyboard 3", true),
                    new DeviceInfo(0x00080381, "Motherboard", false),
                 };
 
