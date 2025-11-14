a860699bd8b3e48734c5443880ca205e4602a3a6
Luca Veronelli <l.veronelli@stem.it>
2025-11-14 16:13:03 +0100

    Commenta nuovi task

--- Core/Interfaces/IButtonPanelTestService.cs
+++ Core/Interfaces/IButtonPanelTestService.cs
@@ -8,8 +8,14 @@ namespace STEMPM.Core.Interfaces
     {
         // Esegue tutti i test per una pulsantiera specifica e restituisce i risultati
         Task<List<ButtonPanelTestResult>> RunAllTestsAsync(ButtonPanelType panelType);
+
+        // Esegue il test dei pulsanti per una pulsantiera specifica
         Task<ButtonPanelTestResult> TestButtonsAsync(ButtonPanel panel);
+
+        // Esegue il test del LED per una pulsantiera specifica
         Task<ButtonPanelTestResult> TestLedAsync(ButtonPanel panel);
+
+        // Esegue il test del buzzer per una pulsantiera specifica
         Task<ButtonPanelTestResult> TestBuzzerAsync(ButtonPanel panel);
     }
 }
