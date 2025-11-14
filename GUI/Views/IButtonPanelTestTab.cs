756e786229060a0d056dcda490a7563c531a1399
Luca Veronelli <l.veronelli@stem.it>
2025-11-14 17:17:16 +0100

    Implementa interfaccia per vista grafica

---
+++ GUI/Views/IButtonPanelTestTab.cs
@@ -0,0 +1,15 @@
+﻿using STEMPM.Core.Enums;
+using STEMPM.Core.Models;
+
+namespace STEMPM.GUI.Views
+{
+    // Interfaccia della vista per la scheda di test delle pulsantiere
+    internal interface IButtonPanelTestTab
+    {
+        event EventHandler OnRunTestsClicked;
+        ButtonPanelType GetSelectedPanelType();
+        void DisplayResults(List<ButtonPanelTestResult> results);
+        void ShowProgress(string message);
+        void ShowError(string message);
+    }
+}
