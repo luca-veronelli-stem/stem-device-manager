9af275d2d79988ca6580a918cb2e3dfc79e294fd
Luca Veronelli <l.veronelli@stem.it>
2025-11-14 15:21:34 +0100

    Rinomina campo nel modello per leggibilità

--- Core/Models/ButtonPanelTestResult.cs
+++ Core/Models/ButtonPanelTestResult.cs
@@ -5,7 +5,7 @@ namespace STEMPM.Core.Models
     // Modello che rappresenta il risultato di un test su una pulsantiera
     internal class ButtonPanelTestResult
     {
-        public ButtonPanelType Type { get; set; }
+        public ButtonPanelType PanelType { get; set; }
         public ButtonPanelTestType TestType { get; set; }
         public bool Passed { get; set; }
         public required string Message { get; set; }
