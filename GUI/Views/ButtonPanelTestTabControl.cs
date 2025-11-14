404f90b54dcdd324643649e61dd3a2de76f27d28
Luca Veronelli <l.veronelli@stem.it>
2025-11-14 17:20:05 +0100

    Implementa user control per la pagina grafica incompleta

---
+++ GUI/Views/ButtonPanelTestTabControl.cs
@@ -0,0 +1,14 @@
+﻿namespace STEMPM.GUI.Views
+{
+    public partial class ButtonPanelTestTabControl : UserControl//, IButtonPanelTestTab
+    {
+        public event EventHandler? OnRunTestsClicked;
+
+        public ButtonPanelTestTabControl()
+        {
+            InitializeComponent();
+        }
+
+        // TODO: Implementare i metodi descritti nell'interfaccia IButtonPanelTestTab
+    }
+}
