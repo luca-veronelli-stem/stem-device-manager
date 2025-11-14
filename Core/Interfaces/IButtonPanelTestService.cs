bc850f0ff62b4042b45e5b8fb07d489b572170bf
Luca Veronelli <l.veronelli@stem.it>
2025-11-14 12:48:01 +0100

    Aggiungi prima interfaccia per il servizio di test su pulsantiere

---
+++ Core/Interfaces/IButtonPanelTestService.cs
@@ -0,0 +1,12 @@
+﻿using STEMPM.Core.Enums;
+using STEMPM.Core.Models;
+
+namespace STEMPM.Core.Interfaces
+{
+    // Interfaccia per il servizio di test delle pulsantiere
+    internal interface IButtonPanelTestService
+    {
+        // Esegue tutti i test per una pulsantiera specifica e restituisce i risultati
+        Task<List<ButtonPanelTestResult>> RunAllTestsAsync(ButtonPanelType panelType);
+    }
+}
