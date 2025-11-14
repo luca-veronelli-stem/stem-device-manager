6569f48bde6afc9ec94d226bd93bbe3a72f06954
Luca Veronelli <l.veronelli@stem.it>
2025-11-14 12:46:59 +0100

    Aggiungi modello per risultato test su pulsantiera

---
+++ Core/Models/ButtonPanelTestResult.cs
@@ -0,0 +1,13 @@
+﻿using STEMPM.Core.Enums;
+
+namespace STEMPM.Core.Models
+{
+    // Modello che rappresenta il risultato di un test su una pulsantiera
+    internal class ButtonPanelTestResult
+    {
+        public ButtonPanelType Type { get; set; }
+        public ButtonPanelTestType TestType { get; set; }
+        public bool Passed { get; set; }
+        public required string Message { get; set; }
+    }
+}
