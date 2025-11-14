0608ae082d5f3154c550127b92d6874fc54389db
Luca Veronelli <l.veronelli@stem.it>
2025-11-14 12:15:21 +0100

    Aggiungi primo modello per pulsantiera

---
+++ Core/Models/ButtonPanel.cs
@@ -0,0 +1,23 @@
+﻿using STEMPM.Core.Enums;
+
+namespace STEMPM.Core.Models
+{
+    internal class ButtonPanel
+    {
+        public ButtonPanelType Type { get; set; }
+        public int ButtonCount { get; set; }
+        public bool HasLed { get; set; }
+        public bool HasBuzzer { get; set; } = true;
+
+        public static ButtonPanel GetByType(ButtonPanelType type)
+        {
+            return type switch
+            {
+                ButtonPanelType.DIS0025205 => new ButtonPanel { Type = type, 
+                    ButtonCount = 4, HasLed = false },
+                _ => new ButtonPanel { Type = type, 
+                    ButtonCount = 8, HasLed = true }
+            };
+        }
+    }
+}
