adf31a61186e2ec23f4647873c3e05e62a53ef31
Luca Veronelli <l.veronelli@stem.it>
2025-11-17 12:07:08 +0100

    Aggiungi commenti all'interfaccia

--- GUI/Views/IButtonPanelTestTab.cs
+++ GUI/Views/IButtonPanelTestTab.cs
@@ -3,13 +3,22 @@ using STEMPM.Core.Models;
 
 namespace STEMPM.GUI.Views
 {
-    // Interfaccia della vista per la scheda di test delle pulsantiere
+    // Interfaccia della vista per la pagina di collaudo pulsantiere
     internal interface IButtonPanelTestTab
     {
+        // Evento scatenato quando l'utente clicca sul pulsante per eseguire il collaudo
         event EventHandler OnRunTestsClicked;
+
+        // Metodo per ottenere il tipo di pulsantiera selezionato dall'utente
         ButtonPanelType GetSelectedPanelType();
+
+        // Metodo per visualizzare i risultati del collaudo
         void DisplayResults(List<ButtonPanelTestResult> results);
+
+        // Metodo per modificare lo stato della vista durante l'esecuzione del collaudo
         void ShowProgress(string message);
+
+        // Metodo per visualizzare eventuali messaggi di errore
         void ShowError(string message);
     }
 }
