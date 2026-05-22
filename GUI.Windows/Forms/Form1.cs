b759030abce351b95dbd1c3f69461b65795470f3
Luca Veronelli <l.veronelli@stem.it>
2026-05-22 08:28:12 +0200

    chore(release): bump to 0.4.0
    - Directory.Build.props: Version / FileVersion / AssemblyVersion -> 0.4.0
    - GUI.Windows/Forms/Form1.cs: Software_Version constant
    - GUI.Windows/Program.cs: @version header + 0.4.0 history line

--- GUI.Windows/Forms/Form1.cs
+++ GUI.Windows/Forms/Form1.cs
@@ -27,7 +27,7 @@ namespace StemPC
         private readonly BLEManager _bleManager;
         private readonly ILogger<Form1> _logger;
 
-        public const string Software_Version = "0.3.0";
+        public const string Software_Version = "0.4.0";
 
         // Canale hardware corrente selezionato. Le mutazioni vengono propagate a
         // ConnectionManager via SwitchToAsync.
